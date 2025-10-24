f97a95bc4127b12eb20a882951393c970a4420a2
AbuZaidM <mohammad.abuzaid@vodafone.com>
2025-10-24 08:44:32 +0300

    initial repo

---
+++ src/main/java/com/zamzam/zamzamapi/ZamzamApiApplication.java
@@ -0,0 +1,13 @@
+package com.zamzam.zamzamapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZamzamApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZamzamApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zamzam/zamzamapi/ZamzamApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zamzam.zamzamapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZamzamApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
